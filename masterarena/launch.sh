b72aa777512d80f601eb65b34157bcf95ad3be2d
Starz0r <starz0r@starz0r.com>
2022-06-20 23:25:54 -0500

    masterarena: maplist override had the wrong name

--- masterarena/launch.sh
+++ masterarena/launch.sh
@@ -8,7 +8,10 @@ X :2 -config ./dummy-1920x1080.conf &
 #sleep 15
 
 cp ./run_server.bat ./server/
-mkdir -R ./server/UDKGame/Config/DedicatedServer1
-cp ./UDKMapList.ini ./server/UDKGame/Config/DedicatedServer1/
+mkdir ./server/UDKGame/
+mkdir ./server/UDKGame/Config/
+mkdir ./server/UDKGame/Config/DedicatedServer1
+cp ./UDKMAMapList.ini ./server/UDKGame/Config/
+cp ./UDKMAMapList.ini ./server/UDKGame/Config/DedicatedServer1/
 cd ./server/
 DISPLAY=:2 wine cmd < run_server.bat
