6b4a79ef66d1bed43c40d15058c24db68f834264
Starz0r <starz0r@starz0r.com>
2022-06-20 23:12:27 -0500

    masterarena: copy maplist to config directory

--- masterarena/launch.sh
+++ masterarena/launch.sh
@@ -8,5 +8,7 @@ X :2 -config ./dummy-1920x1080.conf &
 #sleep 15
 
 cp ./run_server.bat ./server/
+mkdir -R ./server/UDKGame/Config/DedicatedServer1
+cp ./UDKMapList.ini ./server/UDKGame/Config/DedicatedServer1/
 cd ./server/
 DISPLAY=:2 wine cmd < run_server.bat
